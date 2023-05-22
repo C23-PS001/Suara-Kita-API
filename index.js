7fe330a96508528652d52e321bd7722d96b77073
Felix Ferdianto <77229194+xileferdianto08@users.noreply.github.com>
2023-05-22 14:15:51 +0700

    feat: adding a homepage

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ require('dotenv').config();
 const hapi = require("@hapi/hapi");
 const userRoutes = require('./user/routes/user.routes')
 
+const routes1 = [
+  {
+    method: "GET",
+    path: "/",
+    handler: (req, h) => {
+      return "<h1>Welcome to Suara Kita API</h1>";
+    },
+  },
+  {
+    method: "*",
+    path: "/{any*}",
+    handler: (request, h) => {
+      return "Halaman tidak ditemukan";
+    },
+  },
+];
+
 const init = async () => {
   const server = hapi.server({
     port: 8090,
@@ -14,6 +31,7 @@ const init = async () => {
   });
 
   server.route(userRoutes)
+  server.route(routes1);
 
   await server.start();
   console.log(`Server running at ${server.info.uri}`);
